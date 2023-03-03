283e0f5e9e5fcde30b866a8b0f40c3e93b2e823e
Sebastian Mere <sebastian.mere@gmail.com>
2023-03-02 22:19:42 -0300

    Entregable 2

--- App.js
+++ App.js
@@ -1,60 +1,198 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, ScrollView, Button } from 'react-native';
+import {
+  Button,
+  FlatList,
+  Modal,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import React, { useEffect, useState } from "react";
 
 export default function App() {
+  const [itemText, setItemText] = useState("");
+  const [items, setItems] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const onChangeText = (text) => {
+    setItemText(text);
+  };
+
+  const addItemToState = () => {
+    if (itemText.trim() === "") {
+      setShowModal(true);
+      return;
+    }
+    setItems((oldArry) => [...oldArry, { id: Date.now(), value: itemText }]);
+    setItemText("");
+  };
+
+  const openModal = (item) => {
+    setSelectedItem(item);
+    setModalVisible(true);
+  };
+
+  const [showModal, setShowModal] = useState(false);
+
+  const onCancelModal = () => {
+    setModalVisible(!modalVisible);
+  };
+
+  const onDeleteModal = (id) => {
+    setModalVisible(!modalVisible);
+    setItems((oldArry) => oldArry.filter((item) => item.id !== id));
+    setSelectedItem(null);
+  };
+
   return (
     <View style={styles.screen}>
-      <View style={[styles.inputContainer, styles.centrado] }>
-        <TextInput placeholder='Agrega un item' style={styles.input} />
-        <Button title='Add' />
+      <View style={styles.addItemInputContainer}>
+        <TextInput
+          placeholder="Ingresa un item"
+          style={styles.input}
+          onChangeText={onChangeText}
+          value={itemText}
+        />
+        <Button title="Agregar" onPress={addItemToState} />
       </View>
 
-      <View>
-          <ScrollView>
-            <View style={[styles.item, styles.centrado]}>
-              <Text>Elemento 1</Text>
-              <Button title='X' />
-            </View>
-            <View style={[styles.item, styles.centrado]}>
-              <Text>Elemento 2</Text>
-              <Button title='X' />
-            </View>
-            <View style={[styles.item, styles.centrado]}>
-              <Text>Elemento 3</Text>
-              <Button title='X' />
-            </View>
-            <View style={[styles.item, styles.centrado]}>
-              <Text>Elemento 4</Text>
-              <Button title='X' />
-            </View>
-          </ScrollView> 
-      </View>
+      <FlatList
+        data={items}
+        renderItem={(itemData) => (
+          <Pressable
+            style={styles.itemContainer}
+            onPress={() => {
+              openModal(itemData.item);
+            }}
+          >
+            <Text style={styles.item}>{itemData.item.value}</Text>
+          </Pressable>
+        )}
+        keyExtractor={(item) => item.id.toString()}
+      />
 
+      <Modal animationType="fade" transparent={true} visible={showModal}>
+        <View style={styles.modalMainView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Ingrese un valor válido</Text>
+            <Button title="Aceptar" onPress={() => setShowModal(false)} />
+          </View>
+        </View>
+      </Modal>
+
+      <Modal animationType="fade" transparent={true} visible={modalVisible}>
+        <View style={styles.modalMainView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalTitle}>Eliminar Item</Text>
+            <Text style={styles.modalText}>
+              ¿Está seguro que desea eliminar el item{" "}
+              <Text style={styles.modalBoldText}>{selectedItem?.value}</Text>?
+            </Text>
+            <View style={styles.modalActions}>
+              <Pressable
+                style={[styles.button, styles.buttonCancel]}
+                onPress={onCancelModal}
+              >
+                <Text style={styles.buttonText}>Cancelar</Text>
+              </Pressable>
+              <Pressable
+                style={[styles.button, styles.buttonDelete]}
+                onPress={() => {
+                  onDeleteModal(selectedItem.id);
+                }}
+              >
+                <Text style={styles.buttonText}>Eliminar</Text>
+              </Pressable>
+            </View>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   screen: {
-    padding: 30
+    paddingVertical: 50,
+    paddingHorizontal: 30,
+    flex: 1,
   },
-  centrado: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  inputContainer: {
-    marginTop: 30
+  addItemInputContainer: {
+    marginTop: 30,
+    marginBottom: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
   input: {
+    width: "70%",
+    borderBottomColor: "black",
     borderBottomWidth: 1,
-    borderBottomColor: 'black',
-    width: '80%'
+  },
+  itemContainer: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
   },
   item: {
-    paddingTop: 10,
+    padding: 10,
+    textAlign: "center",
   },
-  color: {
-    backgroundColor: 'red'
-  }
+  modalMainView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 10,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  modalTitle: {
+    padding: 10,
+    borderRadius: 5,
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
+  },
+  modalBoldText: {
+    fontWeight: "bold",
+    textDecorationLine: "underline",
+  },
+  modalActions: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  button: {
+    borderRadius: 5,
+    paddingVertical: 10,
+    paddingHorizontal: 25,
+    marginHorizontal: 10,
+  },
+  buttonText: {
+    fontWeight: "bold",
+    color: "white",
+  },
+  buttonCancel: {
+    backgroundColor: "#2196F3",
+  },
+  buttonDelete: {
+    backgroundColor: "#f44336",
+  },
 });
